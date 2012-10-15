887060c9f65865d1cdeda4f1ad75e0fd66a7d169
Lars Yencken <lars@yencken.org>
2012-10-16 10:18:30 +1100

    Version bump to 0.2.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ readme = os.path.join(os.path.dirname(__file__), 'README.md')
 
 setup(
     name='colorific',
-    version='0.2.0',
+    version='0.2.1',
     description='Automatic color palette detection',
     long_description=open(readme).read(),
     author='Lars Yencken',
