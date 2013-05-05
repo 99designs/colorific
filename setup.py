4948e9e155bbea3ea34e390d3de72ab0c992cf8f
Gokmen Gorgen <gokmen@alageek.com>
2013-05-05 15:59:58 +0300

    get dependency list from requirements.pip.

--- setup.py
+++ setup.py
@@ -18,25 +18,24 @@ if version != (2, 7):
     sys.stderr.flush()
     sys.exit(1)
 
-import os
+import os.path
 from setuptools import setup
 
-readme = os.path.join(os.path.dirname(__file__), 'README.rst')
+PROJECT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__)))
+README_FILE_PATH = os.path.join(PROJECT_DIR, 'README.rst')
+REQUIREMENTS_FILE_PATH = os.path.join(PROJECT_DIR, 'requirements.pip')
 
 setup(
     name='colorific',
     version='0.2.1',
     description='Automatic color palette detection',
-    long_description=open(readme).read(),
+    long_description=file(README_FILE_PATH).read(),
     author='Lars Yencken',
     author_email='lars@yencken.org',
     url='http://github.com/99designs/colorific',
     py_modules=['colorific'],
     install_requires=[
-        'Pillow>=1.7.8',
-        'colormath>=1.0.8',
-        'numpy>=1.6.1',
-    ],
+        i.strip() for i in file(REQUIREMENTS_FILE_PATH).readlines()],
     license='ISC',
     entry_points={
         'console_scripts': [
